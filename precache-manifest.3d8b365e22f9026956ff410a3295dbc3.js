ce4c7934d7b53997fac7de9dcfa74ac11eb954db
Shoaib Sharif <sharifshoaib@yahoo.com>
2019-11-11 21:12:18 -0800

    Updates

---
+++ precache-manifest.3d8b365e22f9026956ff410a3295dbc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27d7560751d29a568c18200ceb27688",
+    "url": "/crwnclothing/index.html"
+  },
+  {
+    "revision": "cee3241ca010d1f70500",
+    "url": "/crwnclothing/static/css/main.f9702484.chunk.css"
+  },
+  {
+    "revision": "508168bd4f538a4060d7",
+    "url": "/crwnclothing/static/js/2.3046d0f2.chunk.js"
+  },
+  {
+    "revision": "cee3241ca010d1f70500",
+    "url": "/crwnclothing/static/js/main.9d7caf80.chunk.js"
+  },
+  {
+    "revision": "6f3c4a10c7bf32a2cb02",
+    "url": "/crwnclothing/static/js/runtime-main.1dd8447c.js"
+  }
+]);
